575dbbdc3d6a3bea7985eff85aa703e69effdebb
prjmtstda <105961423+prjmtstda@users.noreply.github.com>
2022-06-21 16:39:32 +0530

    Update Manager1.java

--- src/test/java/pkg/Manager1.java
+++ src/test/java/pkg/Manager1.java
@@ -5,7 +5,7 @@ public class Manager1
 	public static void main(String[] args) 
 	{
 		System.out.println("Testing");
-		System.out.println("QCT");
+		System.out.println("QC Testing");
 		System.out.println("Done");
 	}
 }
